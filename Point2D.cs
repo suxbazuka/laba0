bb4afff97bc1bff72cd5cc7aeae9bb733bb47805
suxbazuka <91901774+suxbazuka@users.noreply.github.com>
2026-09-11 10:19:31 +0700

    Добавлен класс Point2D
    круто

--- Point2D.cs
+++ Point2D.cs
@@ -2,12 +2,8 @@ using System;
 
 namespace ShapesDrawing
 {
-    /// <summary>
-    /// Класс точки с координатами X и Y.
-    /// </summary>
     public class Point2D
     {
-        // Чтение свойств доступно отовсюду, изменение — только изнутри класса
         public int X { get; private set; }
         public int Y { get; private set; }
 
@@ -17,8 +13,6 @@ namespace ShapesDrawing
             Y = y;
         }
 
-        // Смещение координаты на заданную величину (а не простое присваивание,
-        // поэтому реализовано как метод, а не сеттер свойства)
         public void AddX(int x)
         {
             X += x;
